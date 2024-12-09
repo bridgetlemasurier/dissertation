2313d78c1e43fdef92bdd20311b5d39ee84d9387
bridgetlemasurier <s2094420@ed.ac.uk>
2024-12-09 12:29:55 +0000

    sponge exploration

--- data/sponge/README.md
+++ data/sponge/README.md
@@ -1,1 +1,27 @@
+# Sponge Data
+
+## How much data do I have?
+I have 7731 records of sponges 
+
+### Decades
+The data spans from 1894-2023
+
+decade count
+   <dbl> <int>
+1   1890     1
+2   1930     2
+3   1970    10
+4   1980    22
+5   1990    15
+6   2000  1359
+7   2010  5071
+8   2020  1251
+
+The majority of the data is collected between 2000 and 2023
+
+### Morphotype
+In the process of categorising morphotypes 
+
+Of the 7731 observations, only 2313 are identified to phylum Porifera
+
 
--- data/sponge/sponge_exploration.R
+++ data/sponge/sponge_exploration.R
@@ -2,11 +2,14 @@
 
 #Library ----
 library(tidyverse)
+library(maps)
+library(ggthemes)
+library(rnaturalearth)
 
 #sponge data ----
 VMEsponge <- read.csv("data/sponge/VMEsponge.csv") # raw data from ICES
 
-# What year range are we working with?
+# What year range are we working with?----
 tidysponges <- VMEsponge%>%
   mutate(ObsYear = as.numeric(str_extract(ObsDate, "\\d{4}")))
 
@@ -15,8 +18,19 @@ tidysponges%>%
             latest = max(ObsYear))
 
 #working from 1894 - 2023
+hist(tidysponges$ObsYear)  # most are for 2000-2023
 
-# How many observations has taxonomic resolution for morphotype
+tidysponges$decade <- (floor(tidysponges$ObsYear/ 10) * 10)
+
+decade_counts <- tidysponges %>%
+  group_by(decade) %>%
+  summarise(count = n())
+print(decade_counts)
+
+# How much data do I have? ----
+count(tidysponges)
+
+# How many observations has taxonomic resolution for morphotype ----
 
 tidysponges <- tidysponges %>%
   filter(Species != "") %>%  # filter missing species obs out
@@ -28,5 +42,24 @@ spongespecies <- tidysponges%>%
   summarise(count = n())
 
 # Save spongespecies as a CSV file
-write.csv(spongespecies, "spongespecies.csv", row.names = FALSE)
+# write.csv(spongespecies, "spongespecies.csv", row.names = FALSE)
+
+# Map existing records? ----
+
+# Load the world map from rnaturalearth
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+# Create the map
+spongemap <- ggplot(data = world) +
+  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
+  geom_point(data = tidysponges, 
+             aes(x = MiddleLongitude, y = MiddleLatitude),
+             color = "red", alpha = 0.4, size = 2) +
+  coord_sf(xlim = c(-75, 50), ylim = c(30, 85), expand = FALSE) +
+  theme_map() 
+spongemap
+
+# Save the map as a PNG file
+ggsave("sponge_map.png", plot = map, width = 10, height = 8, dpi = 300)
+
 
