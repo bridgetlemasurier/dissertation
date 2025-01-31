c0d39c4db5719c8c71cbcc232cac5059f87327cf
bridgetlemasurier <s2094420@ed.ac.uk>
2025-01-31 17:25:37 +0000

    tidied sponge script so easier to read

--- data/sponge/tidyersponges.R
+++ data/sponge/tidyersponges.R
@@ -1,4 +1,15 @@
-# Sponge tidying
+# Sponge Morphotypes
+
+# aims:
+# 1. tidy sponge data
+#     - classify morphotypes and filter out NAs
+#     - filter years pre 2000
+#     - write new csv for analyses
+
+# 2. map sponge morphs
+#     - map the different morphotypes
+#     - determine extent to clip rasters to
+
 
 # LIBRARY ----
 library(tidyverse)
@@ -10,13 +21,13 @@ library(rnaturalearth)
 VMEsponge <- read.csv("data/sponge/VMEsponge.csv") # raw data from ICES
 
 # Sponge "species" ----
-tidysponges <- VMEsponge %>%
+sponges <- VMEsponge %>%
   filter(Species != "")%>% # filter missing species obs out
   mutate(Species = as.factor(Species),
-       HighestTaxonomicResolution = as.factor(HighestTaxonomicResolution))  # as factor
+         HighestTaxonomicResolution = as.factor(HighestTaxonomicResolution))  # as factor
 
-# morphotype variable ----
-morphsponges <- tidysponges%>%
+# Morphotype variable ----
+sponges <- sponges%>%
   group_by(Species)%>%
   mutate(morphotype = case_when(Species %in% c("Antho (Antho) dichotoma",
                                                "Antho dichotoma",
@@ -74,160 +85,81 @@ morphsponges <- tidysponges%>%
                                 Species %in% c("Pheronema carpenteri",
                                                "Quasillina brevis",
                                                "Stylocordyla borealis") ~ "stipulate"))%>%
-  mutate(morphotype = as.factor(morphotype))
+  mutate(morphotype = as.factor(morphotype))  # make factor
 
-morphspongesNA <- morphsponges%>%
-  filter(!is.na(morphotype))
+sponges <- sponges%>%
+  filter(!is.na(morphotype))  # remove NA morphs 
+
+# remove years pre 2000 ----
+sponges <- sponges %>%
+  mutate(ObsYear = as.numeric(str_extract(ObsDate, "\\d{4}")),  # year of record
+         Decade = as.numeric(floor(ObsYear/ 10) * 10))%>%  # decade of record
+  filter(ObsYear >=  2000)  # filter those taken before 2000
+
+# write new csv ----
+tidyishsponges <- sponges%>%
+  dplyr::select(status, HighestTaxonomicResolution, Species, Ship, SurveyMethod,
+                MiddleLatitude, MiddleLongitude, morphotype, ObsYear, Decade)
+# selected variables needed for analyses
+
+write.csv(tidyishsponges, "data/sponge/tidyishsponge.csv")
 
 # map morphotypes ----
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
 ggplot(data = world) +
   geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = morphspongesNA, 
+  geom_point(data = sponges, 
              aes(x = MiddleLongitude, y = MiddleLatitude, colour = morphotype),
              alpha = 0.4, size = 2) +
   coord_sf(xlim = c(-75, 50), ylim = c(40, 85), expand = FALSE) +
-  theme_map()
+  theme_map() +
+  theme(legend.position = "bottom")
 
-# remove years pre 2000 ----
-morphspongesNA <- morphspongesNA%>%
-  mutate(ObsYear = as.numeric(str_extract(ObsDate, "\\d{4}")),
-         Decade = as.numeric(floor(ObsYear/ 10) * 10))%>%
-  filter(ObsYear >=  2000)
-
-
-# presence and absences ----
-morphsponge_absences <- morphspongesNA%>%
-  filter(Number == 0)
-
-morphsponge_presences <- morphspongesNA%>%
-  filter(Number > 0)
-
-# map morphotype presences and trim extent
+# find extent covering all records ----
 morphmap2 <- ggplot(data = world) +
   geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = morphsponge_presences, 
+  geom_point(data = sponges, 
              aes(x = MiddleLongitude, y = MiddleLatitude, colour = morphotype),
              alpha = 0.4, size = 2) +
   coord_sf(xlim = c(-60, 45), ylim = c(41, 83), expand = FALSE) +
-  theme_map()+
+  theme_map() +
   theme(legend.position = "bottom")
 
 morphmap2
 
-#ggsave("data/sponge/morphmap2.png", plot = morphmap2, width = 10, height = 8, dpi = 300)
+ggsave("data/sponge/morphmap2.png", plot = morphmap2, width = 10, height = 8, dpi = 300)
 
-# presence/absence maps ----
-
-# roughly uk samples
-tidyishsponges <- sponges%>%
-  dplyr::select(status, HighestTaxonomicResolution, Species, Ship, SurveyMethod,
-                MiddleLatitude, MiddleLongitude, morphotype, ObsYear, Decade,
-                presence)
-
-write.csv(tidyishsponges, "data/sponge/tidyishsponge.csv")
-
-ukpresence_map <- ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = sponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = presence),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-20, 10), ylim = c(50, 63), expand = FALSE) +
-  theme_map()
-
-# 'GB': ('United Kingdom', (-7.57216793459, 49.959999905, 1.68153079591, 58.6350001085))
-
-ukpresence_map
-
-# arctic circle samples
-articpresence_map <- ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = sponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = presence),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-5, 50), ylim = c(65, 85), expand = FALSE) +
-  theme_map()
-
-articpresence_map
-
-# check chosen extent contains absences aswell
-
-extpresence_map <- ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = sponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = presence),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-60, 45), ylim = c(41, 83), expand = FALSE) +
-  theme_map() +
-  theme(legend.position = "bottom")
-
-extpresence_map
-
-# ggsave("data/sponge/presencemap.png",
-#       plot = extpresence_map, width = 10, height = 8, dpi = 300)
-
-
+# extent xmin = -60
+#        xmax = 45
+#        ymin = 41
+#        ymax = 83
 # extent to clip rasters to is xlim = c(-60, 45), ylim = c(41, 83)
 #  at least for now, may change to look at specific area
 
-
-# playing with extents
-
-ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = morphsponge_presences, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = morphotype),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-20, 10), ylim = c(50, 65), expand = FALSE) +
-  theme_map()+
-  theme(legend.position = "bottom")
-
-ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = sponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = presence),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-20, 10), ylim = c(50, 65), expand = FALSE) +
-  theme_map() +
-  theme(legend.position = "bottom")
-
-# massive sponges
-massivesponges <- sponges%>%
-  filter(morphotype == "massive")
-
-par(mfrow = c(1, 2))
-
-ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = massivesponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = morphotype),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-75, 50), ylim = c(40, 85), expand = FALSE) +
-  theme_map()+
-  theme(legend.position = "bottom")
-
-ggplot(data = world) +
-  geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
-  geom_point(data = massivesponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = presence),
-             alpha = 0.4, size = 2) +
-  coord_sf(xlim = c(-75, 50), ylim = c(40, 85), expand = FALSE) +
-  theme_map() +
-  theme(legend.position = "bottom")
+## split by morph ----
+morph_labels <- c(
+  arborescent = "Arborescent",  #f9776e
+  caliculate = "Caliculate",  #b8a001
+  massive = "Massive",  #01bb39
+  flabellate = "Flabellate",  #01c0c5
+  stipulate = "Stipulate",  #66a0ff
+  papillate = "Papillate")  #f66ae4
 
 
-## split by morph
 morph_facet <- ggplot(data = world) +
   geom_sf(fill = "lightgrey", color = "gray40", size = 0.3) +
   geom_point(data = sponges, 
-             aes(x = MiddleLongitude, y = MiddleLatitude, colour = morphotype,
-                 shape = presence),
+             aes(x = MiddleLongitude, y = MiddleLatitude, colour = morphotype),
              alpha = 0.4, size = 2) +
   coord_sf(xlim = c(-60, 45), ylim = c(41, 83), expand = FALSE) +
   theme_map()+
-  facet_wrap(vars(morphotype)) +
-  theme(legend.position = "bottom")
+  facet_wrap(vars(morphotype),labeller = labeller(morphotype = morph_labels)) +
+  theme(legend.position = "none")
+
+morph_facet
 
 ggsave("data/sponge/morph_facet.png",
        plot = morph_facet, width = 10, height = 8, dpi = 300)
+
+
